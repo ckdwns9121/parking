9081a5ccb833b5a8e6e5ed1694710b09a23674cb
박창준 <ckdwns9121@naver.com>
2021-01-23 13:55:20 +0900

    test

--- src/pages/main/CouponPage.js
+++ src/pages/main/CouponPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CouponContainer from '../../containers/main/CouponContainer';
 
+//test
 const CouponPage = ({match}) => {
     return <CouponContainer match={match} />;
 };
